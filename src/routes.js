c0e134c5b48c6135640508b9239601f2200c88f2
Alisson <alisson_942@outlook.com>
2019-12-16 18:20:23 -0300

    configurando views

--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,7 @@ const express = require('express')
 const routes = express.Router()
 
 routes.get('/', (req, res) => {
-  return res.send('Olá Mundo')
+  res.render('auth/signup')
 })
 
 module.exports = routes
