cafdc4404dc69413d06aa95c600d07b5c65cbab3
Alisson <alisson_942@outlook.com>
2019-12-18 23:27:33 -0300

    criando sessão do usuario com arquivo fisico

--- src/server.js
+++ src/server.js
@@ -1,33 +1,44 @@
 const express = require('express')
+const session = require('express-session')
+const FileStore = require('session-file-store')(session)
 const nunjucks = require('nunjucks')
 const path = require('path')
 
 class App {
-  constructor () {
-    this.express = express()
-    this.isDev = process.env.NODE_ENV !== 'prodocution'
-    this.middleware()
-    this.views()
-    this.routes()
-  }
+    constructor() {
+        this.express = express()
+        this.isDev = process.env.NODE_ENV !== 'prodocution'
+        this.middleware()
+        this.views()
+        this.routes()
+    }
 
-  middleware () {
-    this.express.use(express.urlencoded({ extended: false }))
-  }
+    middleware() {
+        this.express.use(express.urlencoded({ extended: false }))
+        this.express.use(session({
+            name: 'root',
+            secret: 'MyAppSecret',
+            resave: true,
+            store: new FileStore({
+                path: path.resolve(__dirname, '..', 'tmp', 'sessions')
+            }),
+            saveUninitialized: true
+        }))
+    }
 
-  views () {
-    nunjucks.configure(path.resolve(__dirname, 'app', 'views'), {
-      watch: this.isDev,
-      express: this.express,
-      autoescape: true
-    })
-    this.express.use(express.static(path.resolve(__dirname, 'public')))
-    this.express.set('view engine', 'njk')
-  }
+    views() {
+        nunjucks.configure(path.resolve(__dirname, 'app', 'views'), {
+            watch: this.isDev,
+            express: this.express,
+            autoescape: true
+        })
+        this.express.use(express.static(path.resolve(__dirname, 'public')))
+        this.express.set('view engine', 'njk')
+    }
 
-  routes () {
-    this.express.use(require('./routes'))
-  }
+    routes() {
+        this.express.use(require('./routes'))
+    }
 }
 
 module.exports = new App().express
